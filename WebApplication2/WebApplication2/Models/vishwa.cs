eb6b50b8780f555fb11fd09a2ff5f924604b7a07
VishwaGandhi <vishwaggandhi@yahoo.com>
2018-09-20 12:00:42 +1000

    added

---
+++ WebApplication2/WebApplication2/Models/vishwa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class vishwa
+    {
+        [Display(Name = "Name")]
+        public String name { get; set; }
+
+        [Display(Name = "Bhale")]
+        public String bhale { get; set; } 
+        
+    }
+}
